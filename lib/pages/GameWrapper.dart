ff5a59575c74e621a4318054c3bd4c3a72124fdb
Mert Gül <ibrahimmertgul@gmail.com>
2020-11-07 13:59:50 +0300

    ingame buttons added to wrapper

--- lib/pages/GameWrapper.dart
+++ lib/pages/GameWrapper.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/flutter_spinbox.dart';
 import '../utilities/constant.dart';
 
 class GameWrapper extends StatefulWidget {
@@ -10,35 +12,61 @@ class GameWrapper extends StatefulWidget {
 }
 
 class _GameWrapperState extends State<GameWrapper> {
+  int index;
+  int zar = 6;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Hello"),
-      // ),
       body: SafeArea(
-        child: InteractiveViewer(
-            minScale: 1,
-            maxScale: 5,
-            boundaryMargin: EdgeInsets.all(double.infinity),
-            child: widget.child),
+        child: Stack(
+          children: [
+            InteractiveViewer(
+              minScale: 1,
+              maxScale: 5,
+              boundaryMargin: EdgeInsets.all(double.infinity),
+              child: widget.child,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context)),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    zar = Random().nextInt(6);
+                  });
+                },
+                child: Text("Zar At: $zar"),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                height: 50,
+                width: 150,
+                child: SpinBox(
+                  min: 1,
+                  max: myGame.sides.length.toDouble(),
+                  value: 1,
+                  onChanged: (value) =>
+                      myGame.setPlayNowIndex(value.toInt() + 1),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Navigator.pop(context)),
           FlatButton(
               onPressed: () => myGame.setPlayNowIndex(0), child: Text("0")),
           FlatButton(
               onPressed: () => myGame.setPlayNowIndex(1), child: Text("1")),
-          FlatButton(
-              onPressed: () => myGame.setPlayNowIndex(2), child: Text("2")),
-          FlatButton(
-              onPressed: () => myGame.setPlayNowIndex(3), child: Text("3")),
-          FlatButton(
-              onPressed: () => myGame.setPlayNowIndex(4), child: Text("4")),
         ],
       ),
     );
