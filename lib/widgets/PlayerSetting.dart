b2cf015cd9b8252c382bb45fe9acd9c3798f0957
Mert Gül <ibrahimmertgul@gmail.com>
2020-11-09 16:24:39 +0300

    Lobby page player setter shows players name

--- lib/widgets/PlayerSetting.dart
+++ lib/widgets/PlayerSetting.dart
@@ -21,19 +21,21 @@ class _PlayerSetterState extends State<PlayerSetter> {
   void initState() {
     super.initState();
     selectedColor = widget.color;
+    widget.onColorSelected(
+        new Player(name: "Player ${widget.index + 1}", color: widget.color));
   }
 
   @override
   Widget build(BuildContext context) {
     return OPopupTrigger(
       barrierAnimationDuration: Duration(milliseconds: 400),
-      triggerWidget: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-            height: 100,
-            width: 100,
+      triggerWidget: ListTile(
+        title: Text("Player ${widget.index}"),
+        trailing: Container(
+            height: 50,
+            width: 50,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(20),
               color: selectedColor,
             ),
             child: Icon(
