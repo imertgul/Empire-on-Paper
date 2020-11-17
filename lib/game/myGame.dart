7140eacd400c599e70b7c5560fc293a0f41c461e
Mert Gül <ibrahimmertgul@gmail.com>
2020-11-17 22:34:43 +0300

    sandbox mod added to gamemap

--- lib/game/myGame.dart
+++ lib/game/myGame.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import '../models/player.dart';
-import '../models/gameMap.dart';
+import '../models/Player.dart';
+import '../models/GameMap.dart';
 import 'package:flame/gestures.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flame/game.dart';
@@ -14,15 +14,21 @@ class MyGame extends Game with TapDetector {
   List<Player> sides;
   GameMap myMap;
   String mapJson;
+  bool sandbox;
   List<Color> myColors =
       new List<Color>.filled(2, Colors.black87, growable: true);
 
   MyGame(
-      {@required this.sides, this.playNowIndex = 0, @required this.mapJson}) {
+      {this.sides,
+      this.playNowIndex = 0,
+      @required this.mapJson,
+      this.sandbox = false}) {
     myColors[0] = Colors.black87;
     myColors[1] = Colors.lime;
-    for (var i = 0; i < sides.length; i++) {
-      myColors.add(sides[i].color);
+    if (sides != null) {
+      for (var i = 0; i < sides.length; i++) {
+        myColors.add(sides[i].color);
+      }
     }
     myMap = new GameMap(colors: myColors, screenSize: myScreen);
     // myMap.createMap(fill: 1); //or leave blank to random
@@ -53,7 +59,10 @@ class MyGame extends Game with TapDetector {
   @override
   void onTapUp(TapUpDetails d) {
     print("tap up");
-    myMap.tap(d.localPosition.dx, d.localPosition.dy, playNowIndex, base: 0);
+    if (sandbox) {
+      myMap.tap(d.localPosition.dx, d.localPosition.dy, playNowIndex);
+    } else
+      myMap.tap(d.localPosition.dx, d.localPosition.dy, playNowIndex, base: 0);
   }
 
   @override
